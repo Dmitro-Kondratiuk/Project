efa631cc73dbcd98c031b24906b8982f96c046dd
Dmitry Kondratyuk <57353129+Dmitro-Kondratiuk@users.noreply.github.com>
2023-05-29 14:25:45 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -15,26 +15,6 @@ $this->title = 'My Yii Application';
                 <div class="shape-icon bounce-animate">
                     <img src="img/slider/shape-icon.png" alt="">
                 </div>
-<!--                <div class="container-fluid">-->
-<!--                    <div class="row">-->
-<!--                        <div class="col-xl-7">-->
-<!--                            <div class="slide-content">-->
-<!--                                <span data-animation="fadeInRight" data-delay=".2s">Furniture Collection</span>-->
-<!--                                <h1 data-animation="fadeInUp" data-delay=".5s">Synnes Dining-->
-<!--                                    Chair Upholstered</h1>-->
-<!--                                <div class="slide-btn">-->
-<!--                                    <a class="btn theme-btn" href="shop.html" data-animation="fadeInLeft" data-delay=".7s">shop now</a>-->
-<!--                                    <a class="btn white-btn" href="shop.html" data-animation="fadeInRight" data-delay=".9s">category</a>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-xl-5">-->
-<!--                            <div class="slide-shape1" data-animation="bounceInRight" data-delay=".9s"><img src="img/slider/shape1.png" alt=""></div>-->
-<!--                            <div class="slide-shape2" data-animation="bounceInRight" data-delay="1.2s"><img src="img/slider/shape2.png" alt=""></div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
             <div class="single-slider slide-1-style slide-height d-flex align-items-center" data-background="img/slider/slide1-1.jpg">
                 <div class="shape-title bounce-animate">
                     <h2>UX</h2>
